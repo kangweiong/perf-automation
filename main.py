f43a00bc7c517c1ac71ce6685ed367b15fc4e99d
kangweiong <kangggwei@gmail.com>
2024-09-02 00:29:49 +0800

    include additional metrics

--- main.py
+++ main.py
@@ -49,7 +49,19 @@ def main(region:str):
     Query(3118, params={"region": region, "timezone": timezone, "date": date}),
     Query(3119, params={"region": region, "timezone": timezone, "date": date}),
     Query(3120, params={"region": region, "timezone": timezone, "date": date}),
-    Query(3121, params={"region": region, "timezone": timezone, "date": date}),],
+    Query(3121, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3122, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3123, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3124, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3125, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3126, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3127, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3128, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3129, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3130, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3131, params={"region": region_id, "timezone": timezone, "date": date}),
+    Query(3132, params={"region": region, "timezone": timezone, "date": date}),
+    Query(3133, params={"region": region, "timezone": timezone, "date": date}),],
   ]
 
   for query_list in queries:
@@ -60,6 +72,12 @@ def main(region:str):
   bq3 = redash.get_result(2669)
   bq4 = redash.get_result(2670)
   bq5 = redash.get_result(2671)
+  bq6 = redash.get_result(3124)
+  bq7 = redash.get_result(3125)
+  bq8 = redash.get_result(3126)
+  bq9 = redash.get_result(3129)
+  bq10 = redash.get_result(3130)
+  bq11 = redash.get_result(3131)
 
   df1 = redash.get_result(3106)
   df2 = redash.get_result(3107)
@@ -77,6 +95,12 @@ def main(region:str):
   df14 = redash.get_result(3119)
   df15 = redash.get_result(3120)
   df16 = redash.get_result(3121)
+  df17 = redash.get_result(3122)
+  df18 = redash.get_result(3123)
+  df19 = redash.get_result(3127)
+  df20 = redash.get_result(3128)
+  df21 = redash.get_result(3132)
+  df22 = redash.get_result(3133)
 
   df = pd.DataFrame()
 
@@ -192,6 +216,78 @@ def main(region:str):
   df['r_d_ratio'] = df.rider_mau/df.driver_mau
 
   df['average_eta'] = df14.daily_median_eta
+
+  df = df.copy()
+
+  df['bike_mau'] = bq6.online_bike_count
+  df['completed_bike'] = df1.completed_bikes
+  df['bike_online_daily'] = bq6.online_bike_daily
+  df['pinged_bike_daily'] = bq7.pinged_bike_daily
+  df['completed_bike_daily'] = df17.completed_bike_daily
+
+  df['bike_online_mau'] = df.bike_online_daily/df.bike_mau
+  df['bike_completed_online'] = df.completed_bike_daily/df.bike_online_daily
+  df['bike_online_no_complete'] = df.bike_online_daily-df.completed_bike_daily
+
+  df['ride_per_bike'] = df17.ride_per_bike
+
+  df['bike_sign_up'] = df11.bike_sign_up
+
+  df['bike_ft_all_time'] = df12.bike_all_time
+  df['bike_ft_same_month'] = df12.bike_same_month
+  df['bike_sign_up_activation_rate'] = df.bike_ft_same_month/df.bike_sign_up
+
+  df['bike_approved_activation_rate'] = df.bike_ft_same_month/df11.bike_same_month_approved
+  df['bike_approved'] = df11.bike_approved
+  df['bike_same_month_approved'] = df11.bike_same_month_approved
+
+  df['bike_average_online_hours'] = bq8.avg_online_hour
+  df['bike_average_utilisation_hours'] = df18.avg_utilisation_hours
+
+  df['ping_per_bike_daily'] = bq7.ping_per_bike_daily
+
+  df['bike_waiting_before_cancel'] = df9.avg_waiting_time_bike_cxl
+  df['bike_cancellation_rate'] = df1.bike_cancel/df1.bike_demand*100
+
+  df['_4w_mau'] = bq9.online_4w_count
+  df['completed_4w'] = df1.completed_4w
+  df['_4w_online_daily'] = bq9.online_4w_daily
+  df['pinged_4w_daily'] = bq10.pinged_4w_daily
+  df['completed_4w_daily'] = df19.completed_4w_daily
+
+  df['_4w_online_mau'] = df._4w_online_daily/df._4w_mau
+  df['_4w_completed_online'] = df.completed_4w_daily/df._4w_online_daily
+  df['_4w_online_no_complete'] = df._4w_online_daily-df.completed_4w_daily
+
+  df['ride_per_4w'] = df19.ride_per_4w
+
+  df['_4w_sign_up'] = df11._4w_sign_up
+
+  df['_4w_ft_all_time'] = df12._4w_all_time
+  df['_4w_ft_same_month'] = df12._4w_same_month
+  df['_4w_sign_up_activation_rate'] = df._4w_ft_same_month/df._4w_sign_up
+
+  df['_4w_approved_activation_rate'] = df._4w_ft_same_month/df11._4w_same_month_approved
+  df['_4w_approved'] = df11._4w_approved
+  df['_4w_same_month_approved'] = df11._4w_same_month_approved
+
+  df['_4w_average_online_hours'] = bq11.avg_online_hour
+  df['_4w_average_utilisation_hours'] = df20.avg_utilisation_hours
+
+  df['ping_per_4w_daily'] = bq10.ping_per_4w_daily
+
+  df['_4w_waiting_before_cancel'] = df9.avg_waiting_time_4w_cxl
+  df['_4w_cancellation_rate'] = df1._4w_cancel/df1._4w_demand*100
+
+  df['bike_cater_rate'] = df.bike_rides/df8.bike_unique
+  df['bike_r_d_ratio'] = df.rider_mau/df.bike_mau
+
+  df['average_bike_eta'] = df21.daily_median_eta
+
+  df['_4w_cater_rate'] = (df.rides-df.bike_rides)/df8._4w_unique
+  df['_4w_r_d_ratio'] = df.rider_mau/df._4w_mau
+
+  df['average_4w_eta'] = df22.daily_median_eta
 
   df = df.T
   df.columns = [f"{output_date}"]
